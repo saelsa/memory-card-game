8d98137ceb22faf8718c1308cc4b36cb9932b9e8
saelsa <saelsa@gmx.net>
2018-03-27 17:52:49 +0300

    feat: Adding resizable and flippable cards

--- js/app.js
+++ js/app.js
@@ -1,31 +1,44 @@
 $(function() {
 
-    //array that holds the values for the memory cards
+    //DECLARATION OF VARIABLES
     const memoryBoard = $('#memory-game');
 
+    //array that holds the values for the memory cards
     let cardArray = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H'];
 
 
-
+    //on page load
     createBoard();
 
 
 
+    //EVENT TRIGGERS
+    $( '.card' ).click(function(event) {
+        flipCard(event.target);
+    });
 
 
 
+    //FUNCTIONS
 
+    //function to flip cards
+    function flipCard(element) {
+        $(element).parent().toggleClass('flipped');
+
+    }
+
+    //function to create the memory board
     function createBoard() {
 
-            cardArray = shuffle(cardArray);
+        cardArray = shuffle(cardArray);
 
-            for (i = 1; i <= cardArray.length; i++) {
-            memoryBoard.append($(`<div class='card' data-card-type='${cardArray[i-1]}'><div class='front'></div><div class='back'> ${cardArray[i-1]} </div></div>'`));
-            }
+        for (i = 1; i <= cardArray.length; i++) {
+        memoryBoard.append($(`<div class='container'><div class='card' data-card-type='${cardArray[i-1]}'><figure class='front'></figure><figure class='back'> ${cardArray[i-1]} </figure></div></div>'`));
+        }
     };
 
 
-    // Shuffle function from http://stackoverflow.com/a/2450976
+    // shuffle function from http://stackoverflow.com/a/2450976
     function shuffle(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
 
